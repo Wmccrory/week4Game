2b0edadc19efd09a485c5d75b92c4b1cb0aa93d2
Will <wmccrory@gmail.com>
2018-01-23 18:46:44 -0600

    Battle mechanics added. Death mechanics in progress

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -26,18 +26,16 @@ var enemySelect = false;
 
 //Hero stats
 var heroName;
-var heroMaxHealth;
+var heroHealth;
 var heroAttack;
-var heroCounterAttack;
 var heroPortrait;
 var heroBattlePortrait;
 var wins;
 
 //Enemy stats
 var enemyName;
-var enemyMaxHealth;
+var enemyHealth;
 var enemyAttack;
-var enemyCounterAttack;
 var enemyPortrait;
 var enemyBattlePortrait;
 
@@ -46,7 +44,7 @@ var enemyBattlePortrait;
 
 var kidA = {
 	"name": "Jedi Knight Anakin Skywalker",
-	"maxHealth": 100,
+	"maxHealth": 1000,
 	"attack": 100,
 	"attackDesc": "Sith Rage (Here I go killin' younglings again): ",
 	"counterAttack": 50,
@@ -57,7 +55,7 @@ var kidA = {
 
 var kidB = {
 	"name": "Sors Bandeam",
-	"maxHealth": 100,
+	"maxHealth": 1000,
 	"attack": 100,
 	"attackDesc": "Resigned determination: ",
 	"counterAttack": 50,
@@ -68,7 +66,7 @@ var kidB = {
 
 var kidC = {
 	"name": "Tusken Kid (Force Ghost)",
-	"maxHealth": 100,
+	"maxHealth": 1000,
 	"attack": 100,
 	"attackDesc": "Power of Ghost of Younglings Past: ",
 	"counterAttack": 50,
@@ -79,10 +77,10 @@ var kidC = {
 
 var kidD = {
 	"name": "Smug Jedi Pre-teen",
-	"maxHealth": 100,
+	"maxHealth": 1000,
 	"attack": 100,
 	"attackDesc": "Smug adolescant superiority: ",
-	"counterAttack": 50,
+	"counterAttack": 600,
 	"selectPortrait": "#kiddportrait",
 	"battlePortrait": "#kiddbattle",
 	"wins": 0,
@@ -102,6 +100,7 @@ function heroSelect() {
 		$("#kidabattle").fadeIn(1000);
 		hero = kidA;
 		heroAttack = kidA.attack;
+		heroHealth = kidA.maxHealth;
 		}
 	else if (hero === "kidbportrait")
 		{	
@@ -110,6 +109,7 @@ function heroSelect() {
 		$("#kidbbattle").fadeIn(1000);
 		hero = kidB;
 		heroAttack = kidB.attack;
+		heroHealth = kidB.maxHealth;
 		}
 	else if (hero === "kidcportrait")
 		{	
@@ -118,6 +118,7 @@ function heroSelect() {
 		$("#kidcbattle").fadeIn(1000);
 		hero = kidC;
 		heroAttack = kidC.attack;
+		heroHealth = kidC.maxHealth;
 		}
 	else if (hero === "kiddportrait")
 		{	
@@ -126,6 +127,7 @@ function heroSelect() {
 		$("#kiddbattle").fadeIn(1000);
 		hero = kidD;
 		heroAttack = kidD.attack;
+		heroHealth = kidD.maxHealth;
 		}
 	else
 		{
@@ -141,8 +143,8 @@ function opponentSelect() {
 		$("#kidaportrait").fadeOut(1000);
 		$("#kidabattle").fadeIn(1000);
 		enemy = kidA;
-		enemyAttack = kidA.attack;
-		console.log(heroAttack);
+		enemyAttack = kidA.counterAttack;
+		enemyHealth = kidA.maxHealth;
 		}
 	else if (enemy === "kidbportrait")
 		{	
@@ -150,7 +152,8 @@ function opponentSelect() {
 		$("#kidbportrait").fadeOut(1000);
 		$("#kidbbattle").fadeIn(1000);
 		enemy = kidB;
-		enemyAttack = kidB.attack;
+		enemyAttack = kidB.counterAttack;
+		enemyHealth = kidB.maxHealth;
 		}
 	else if (enemy === "kidcportrait")
 		{	
@@ -158,7 +161,8 @@ function opponentSelect() {
 		$("#kidcportrait").fadeOut(1000);
 		$("#kidcbattle").fadeIn(1000);
 		enemy = kidC;
-		enemyAttack = kidC.attack;
+		enemyAttack = kidC.counterAttack;
+		enemyHealth = kidC.maxHealth;
 		}
 	else if (enemy === "kiddportrait")
 		{	
@@ -166,13 +170,48 @@ function opponentSelect() {
 		$("#kiddportrait").fadeOut(1000);
 		$("#kiddbattle").fadeIn(1000);
 		enemy = kidD;
-		enemyAttack = kidD.attack;
+		enemyAttack = kidD.counterAttack;
+		enemyHealth = kidD.maxHealth;
 		}
 	else
 		{
 		console.log("I don't know");
 		}		
 }
+
+//Attack function//
+function attackMechanics() {
+	enemyHealth = enemyHealth - heroAttack;
+	if (enemyHealth < 1) 
+	{
+		hero.wins++;
+		enemySelect = false;
+		$(enemy.battlePortrait).fadeOut(1000);
+		console.log(enemy.battlePortrait);
+		$("#opponentTwoName").html("<s>" + enemy.name + "</s>");
+		$("#opponentTwoHealth").text("Opponent has been slain");
+		$("#opponentTwoForce").text("Choose another for the slaughter");
+	}
+	else 
+	{
+		return $("#opponentTwoHealth").text("Health: " + enemyHealth);
+	}
+}
+
+//CounterAttack function
+function counterAttackMechanics () {
+	heroHealth = heroHealth - enemyAttack;
+	if (heroHealth < 1) 
+	{
+		console.log("Hero has died");
+	}
+	else
+	{
+		return $("#opponentOneHealth").text("Health: " + heroHealth);
+	}
+}
+
+
 
    ///////////////////////////////////////////////////////////////////////////////////////////////////
   ///////////////////////////////////////////////////////////////////////////////////////////////////
@@ -194,7 +233,7 @@ $(".pickingPortrait").on("click", function() {
 		hero = this.id;
 		heroSelect();
 		$("#opponentOneName").text(hero.name);
-		$("#opponentOneHealth").text("Health: " + hero.maxHealth);
+		$("#opponentOneHealth").text("Health: " + heroHealth);
 		$("#opponentOneForce").text(hero.attackDesc + heroAttack);
 		console.log(hero);
 		}
@@ -204,103 +243,25 @@ $(".pickingPortrait").on("click", function() {
 		enemy = this.id;
 		opponentSelect();
 		$("#opponentTwoName").text(enemy.name);
-		$("#opponentTwoHealth").text("Health: " + enemy.maxHealth);
+		$("#opponentTwoHealth").text("Health: " + enemyHealth);
 		$("#opponentTwoForce").text(enemy.attackDesc + enemyAttack);
 		console.log(enemy);
 		}
 });
 
+ //////////////////
+//Fighter Attack//
 
-
-
+$("#attackBtn").on("click", function() {
+	if ((playerSelect === false) || (enemySelect === false))
+	{
+		console.log("Not ready for the slaughter");
+	}
+	else
+	{
+	attackMechanics();
+	counterAttackMechanics();
+	}
 });
 
- //////////////////
-//Fighter select//
-
-// $("#kidaportrait").click(function kidAfunction() {
-// 	// $("#kidaportrait").fadeOut(1000);
-// 	// $("#kidabattle").fadeIn(1000);
-// 		if ((opponentOneSelect === true) && (opponentTwoSelect === true)) 
-// 		{
-// 			console.log("Already selected opponents");
-			
-// 		} 
-// 		else if (opponentTwoSelect !== true) 
-// 		{
-// 			console.log("elseif running");
-// 			$("#kidabattle").appendTo("#opponentOneBattle");
-// 			$("#opponentOneName").text("Jedi Knight Anakin Skywalker");
-// 			$("#opponentOneHealth").text("Health: " + "100/100");	
-// 			$("#kidaportrait").fadeOut(1000);
-// 	$("#kidabattle").fadeIn(1000);		
-// 		} 
-// 		else 
-// 		{
-// 			$("#kidabattle").appendTo("#opponentTwoBattle");
-// 			$("#opponentTwoName").text("Jedi Knight Anakin Skywalker");
-// 			$("#opponentTwoHealth").text("Health: " + "100/100");
-// 			$("#kidaportrait").fadeOut(1000);
-// 	$("#kidabattle").fadeIn(1000);
-// 		}
-// });
-
-// $("#kidbportrait").click(function() {
-// 	$("#kidbportrait").fadeOut(1000);
-// 	$("#kidbbattle").fadeIn(1000);
-// 		if (opponentTwoSelect !== true) 
-// 		{
-// 			$("#kidbbattle").appendTo("#opponentOneBattle");
-// 			$("#opponentOneName").text("Sors Bandeam");
-// 			$("#opponentOneHealth").text("Health: " + "100/100");
-// 			opponentOneSelect = true;
-// 			opponentTwoSelect = true;
-// 			console.log("opponent 1 select: "+ opponentOneSelect);
-// 			console.log(opponentTwoSelect);
-// 		} 
-// 		else 
-// 		{
-// 			$("#kidbbattle").appendTo("#opponentTwoBattle");
-// 			$("#opponentTwoName").text("Sors Bandeam");
-// 			$("#opponentTwoHealth").text("Health: " + "100/100");
-// 			opponentOneSelect = true;
-// 			opponenTwoSelect = true;
-// 			console.log(opponentOneSelect);
-// 			console.log(opponentTwoSelect);
-// 		}
-// });
-
-// $("#kidcportrait").click(function() {
-// 	$("#kidcportrait").fadeOut(1000);
-// 	$("#kidcbattle").fadeIn(1000);
-// 		if (opponentTwoSelect !== true) 
-// 		{
-// 			$("#kidcbattle").appendTo("#opponentOneBattle");
-// 			$("#opponentOneName").text("Tusken Kid (Force Ghost)");
-// 			$("#opponentOneHealth").text("Health: " + "100/100");
-// 		} 
-// 		else 
-// 		{
-// 			$("#kidcbattle").appendTo("#opponentTwoBattle");
-// 			$("#opponentTwoName").text("Tusken Kid (Force Ghost)");
-// 			$("#opponentTwoHealth").text("Health: " + "100/100");
-// 		}
-
-// });
-
-// $("#kiddportrait").click(function() {
-// 	$("#kiddportrait").fadeOut(1000);
-// 	$("#kiddbattle").fadeIn(1000);
-// 		if (opponentTwoSelect !== true) 
-// 		{
-// 			$("#kiddbattle").appendTo("#opponentOneBattle");
-// 			$("#opponentOneName").text("Smug Jedi Pre-teen");
-// 			$("#opponentOneHealth").text("Health: " + "100/100");
-// 		} 
-// 		else 
-// 		{
-// 			$("#kiddbattle").appendTo("#opponentTwoBattle");
-// 			$("#opponentTwoName").text("Smug Jedi Pre-teen");
-// 			$("#opponentTwoHealth").text("Health: " + "100/100");
-// 		}
-// });
+}); //End of code execution
